11697197a5dfb0bd416c57c28afb198983aedd33
pizzamoeger <osshannahsophie@gmail.com>
2025-06-06 20:04:33 +0200

    login and logout

--- scripts/auth.js
+++ scripts/auth.js
@@ -16,3 +16,29 @@ signUpForm.addEventListener("submit", (e)=> {
         signUpForm.reset();
     });
 });
+
+// log out
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("user logged out");
+    });
+});
+
+// login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm["login-email"].value;
+    const password = loginForm["login-password"].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        const modal = document.querySelector("#modal-login");
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
